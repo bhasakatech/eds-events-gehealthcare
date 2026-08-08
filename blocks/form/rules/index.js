511de836b199237f2ab2ca6e089799d49358c22b
bhasakatech <chamu.chagani@bhasaka.com>
2026-08-08 13:26:16 +0530

    lint formate fixes

--- blocks/form/rules/index.js
+++ blocks/form/rules/index.js
@@ -446,14 +446,16 @@ async function initializeRuleEngineWorker(formDef, renderHTMLForm) {
     if (response?.form) {
       // Wire DOM events to the model, same as the worker path does after restoreState.
       // dataset.id must be set before loadRuleEngine keys into formModels.
+
       response.form.dataset.id = formDef.id;
+
       await loadRuleEngine(
-      formState,
-      response.form,
-      response.captcha,
-      response.generateFormRendition,
-      data,
-    );
+        formState,
+        response.form,
+        response.captcha,
+        response.generateFormRendition,
+        data,
+      );
     }
     return { ...response, afbForm: formModels[formDef.id] ?? afbForm };
   }
